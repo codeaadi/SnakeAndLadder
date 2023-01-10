3f40093e4e21397d34a9a626ef7c615f5ae42b97
codeaadi <adityadubey362@gmail.com>
2023-01-10 13:59:36 +0530

    Added:Program for single player input

---
+++ com/bridgelabz/day4/snakeandladder/UC_1_SinglePlayer.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.day4.snakeandladder;
+
+public class UC_1_SinglePlayer {
+    public static void main(String[] args) {
+        int player;
+        int position = 0;
+    }
+}
