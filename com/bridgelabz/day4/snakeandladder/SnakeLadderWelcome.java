9a8abff7fcd6a67e1152508e9f329fc1f52677cd
codeaadi <adityadubey362@gmail.com>
2023-01-10 13:54:35 +0530

    Added:Welcome Message For Snake And Ladder Program

---
+++ com/bridgelabz/day4/snakeandladder/SnakeLadderWelcome.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.day4.snakeandladder;
+
+public class SnakeLadderWelcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Snake And Ladder Program");
+    }
+}
