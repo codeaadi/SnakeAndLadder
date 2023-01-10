62f11cc76c2c09a26e4af7d99e933bd2c13f2278
codeaadi <adityadubey362@gmail.com>
2023-01-10 14:03:35 +0530

    Added:Program for dice Rolling

---
+++ com/bridgelabz/day4/snakeandladder/UC_2_DiceRoll.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.day4.snakeandladder;
+
+import java.util.Random;
+
+public class UC_2_DiceRoll {
+    public static void main(String[] args) {
+        int player;
+        int position=0;
+        Random random= new Random();
+        int diceRoll = 1+random.nextInt(6);
+    }
+}
